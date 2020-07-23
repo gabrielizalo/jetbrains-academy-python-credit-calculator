504cdf0f1ebf8a762b40be4036b79a5e72b10f20
Gabriel Porras <ghporras@gmail.com>
2020-07-23 18:01:09 -0500

    feat: Work on project. Stage 2/4

--- Credit Calculator/task/creditcalc/creditcalc.py
+++ Credit Calculator/task/creditcalc/creditcalc.py
@@ -1,12 +1,50 @@
-credit_principal = 'Credit principal: 1000'
-final_output = 'The credit has been repaid!'
-first_month = 'Month 1: paid out 250'
-second_month = 'Month 2: paid out 250'
-third_month = 'Month 3: paid out 500'
+import math
 
-# write your code here
-print(credit_principal)
-print(first_month)
-print(second_month)
-print(third_month)
-print(final_output)
+# Globals
+credit_main = 0
+calculate_type = None
+
+
+# Functions
+def begin():
+    global credit_main
+    global calculate_type
+
+    print('Enter the credit principal:')
+    credit_main = int(input())
+
+    print('What do you want to calculate?')
+    print('type "m" - for count of months,')
+    print('type "p" - for monthly payment:')
+    while calculate_type != 'm' and calculate_type != 'p':
+        calculate_type = input()
+
+
+def calculate_months():
+    global credit_main
+    print('Enter monthly payment:')
+    payment = int(input())
+    months = int(credit_main / payment)
+    if credit_main % payment > 0:
+        months += 1
+    print(f'It takes {months} to repay the credit')
+
+
+def calculate_payment():
+    global credit_main
+    print('Enter count of months:')
+    months = int(input())
+    payment = math.ceil(credit_main / months)
+    str_payment = f'Your monthly payment = {payment}'
+    if payment * months != credit_main:
+        difference = payment + (credit_main - (payment * months))
+        str_payment += f' with last month payment = {difference}.'
+    print(str_payment)
+
+
+# Main
+begin()
+if calculate_type == 'm':
+    calculate_months()
+else:
+    calculate_payment()
