f9c53c426cd18fe5f1b3e7f93cc7ed4492e7511e
Gabriel Porras <ghporras@gmail.com>
2020-08-05 15:04:49 -0500

    feat: Math functions exercise

--- Problems/Octahedron/task.py
+++ Problems/Octahedron/task.py
@@ -1,3 +1,7 @@
 import math
 
 edge_length = int(input())
+octahedron = 2 * (math.sqrt(3) * math.pow(edge_length, 2))
+volume = (1 / 3) * math.sqrt(2) * math.pow(edge_length, 3)
+
+print('{0} {1}'.format(round(octahedron, 2), round(volume, 2)))
